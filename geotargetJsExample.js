375ee2bf66e3446e3a3a7f7ab87022a2fa4a52bd
Brian Fohn <brian.fohn@wpengine.com>
2018-05-21 00:57:47 -0500

    Create geotargetJsExample.js

---
+++ geotargetJsExample.js
@@ -0,0 +1,4 @@
+var euCountryCodes = ['US'];
+if (euCountryCodes.includes(window.geotarget.countryCode)) {
+    alert('Our company will comply with GDPR by 25 May 2018. To find out more visit: https://www.eugdpr.org/');
+}
